74330c82e1c047c5d4790efcb7f5e060c11794be
SabaBurduli888 <sabagg790@gmail.com>
2025-06-29 07:31:00 +0400

    adding fixed source codes

--- Domain/Entities/BugChaseEntities/LeaderboardEntry.cs
+++ Domain/Entities/BugChaseEntities/LeaderboardEntry.cs
@@ -1,6 +1,7 @@
 ﻿namespace Domain.Entities.BugChaseEntities;
 
 public class LeaderboardEntry
-{
-    
+{   
+    public string Username { get; set; } = default!;
+    public int Score { get; set; }
 }
