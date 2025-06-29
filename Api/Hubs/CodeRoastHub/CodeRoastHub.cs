dd7f3b6fc3459e6c367eba9cbd54067944b96ce0
SabaBurduli888 <sabagg790@gmail.com>
2025-06-29 07:30:12 +0400

    adding fixed source codes

--- Api/Hubs/CodeRoastHub/CodeRoastHub.cs
+++ Api/Hubs/CodeRoastHub/CodeRoastHub.cs
@@ -5,15 +5,35 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Api.Hubs.CodeRoastHub;
 
-public class CodeRoastHub : Hub { }
+public class CodeRoastHub : Hub
+{   
+    public static class CodeRoastEndPointDefinition
+    {
+        public static void MapCodeRoastEndpoints(/*this*/ IEndpointRouteBuilder app)
+        {
+            app.MapPost("/api/roast", async (
+                    [FromBody] RoastCodeHandler command,
+                    ISender sender) =>
+                {
+                    var response = await sender.Send(command);
+                    return Results.Ok(response);
+                })
+                .AddEndpointFilter<CodeRoastExceptionFilter>()
+                .WithName("RoastCode");
+
+            app.MapHub<CodeRoastHub>("/ws/coderoast");
+        }
+    }
+}
 
 // CodeRoastEndPointDefinition.cs
-public static class CodeRoastEndPointDefinition
+
+/*public static class CodeRoastEndPointDefinition
 {
     public static void MapCodeRoastEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapPost("/api/roast", async (
-                [FromBody] RoastCodeCommand command,
+                [FromBody] RoastCodeHandler command,
                 ISender sender) =>
             {
                 var response = await sender.Send(command);
@@ -24,4 +44,4 @@ public static class CodeRoastEndPointDefinition
 
         app.MapHub<CodeRoastHub>("/ws/coderoast");
     }
-}
+}*/
