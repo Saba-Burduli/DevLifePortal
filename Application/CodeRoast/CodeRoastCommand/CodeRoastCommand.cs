3537c2e4c67da0955956fe25ad1b0e9465605a27
SabaBurduli888 <sabagg790@gmail.com>
2025-06-29 07:30:06 +0400

    adding fixed source codes

--- Application/CodeRoast/CodeRoastCommand/CodeRoastCommand.cs
+++ Application/CodeRoast/CodeRoastCommand/CodeRoastCommand.cs
@@ -4,14 +4,17 @@ using MediatR;
 
 namespace Application.CodeRoast.RoastCodeCommand;
 
-public record RoastCodeCommand(string CodeSnippet) : IRequest<CodeRoastResponse>;
+public record CodeRoastCommand(string CodeSnippet) : IRequest<CodeRoastResponse>
+{
+    
+}
 
-public class RoastCodeHandler : IRequestHandler<RoastCodeCommand, CodeRoastResponse>
+public class RoastCodeHandler : IRequestHandler<CodeRoastCommand, CodeRoastResponse>
 {
     private readonly ICodeRoastRepository _roastService;
     public RoastCodeHandler(ICodeRoastRepository roastService) => _roastService = roastService;
 
-    public async Task<CodeRoastResponse> Handle(RoastCodeCommand request, CancellationToken cancellationToken)
+    public async Task<CodeRoastResponse> Handle(CodeRoastCommand request, CancellationToken cancellationToken)
     {
         return await _roastService.RoastCodeAsync(new CodeRoastRequest(request.CodeSnippet));
     }
