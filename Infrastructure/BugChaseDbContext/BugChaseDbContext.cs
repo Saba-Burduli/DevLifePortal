73993545ac7c57f330f069a517371577886b3852
SabaBurduli888 <sabagg790@gmail.com>
2025-06-29 07:29:27 +0400

    fixing errors

--- Infrastructure/BugChaseDbContext/BugChaseDbContext.cs
+++ Infrastructure/BugChaseDbContext/BugChaseDbContext.cs
@@ -7,4 +7,5 @@ public class BugChaseDbContext : DbContext
     public BugChaseDbContext(DbContextOptions<BugChaseDbContext> options) : base(options) {}
     
     public DbSet<BugChasePlayer> Players => Set<BugChasePlayer>();
+    public DbSet<LeaderboardEntry> LeaderBoards { get; set; }
 }
